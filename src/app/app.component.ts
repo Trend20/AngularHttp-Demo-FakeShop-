340e7e0956ec6afb40bf0b7e361c08aa43f78c53
DevEnock <enockomondi305@gmail.com>
2021-12-14 12:52:59 +0300

    adding http client and data fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'api-demo';
+
+  data:any = []
+  // private
+  constructor(private httpClient: HttpClient){}
+
+  getData(){
+    const url = 'https://jsonplaceholder.typicode.com/photos?albumId=1';
+    this.httpClient.get(url).subscribe((response) =>{
+      this.data = response;
+      console.log(this.data);
+    });
+  }
+
+  ngOnInit(): void {
+      this.getData();
+  }
 }
