c60d2a52072fcdb1e571c56d8ae4018266f16551
DevEnock <enockomondi305@gmail.com>
2021-12-14 14:08:49 +0300

    adding the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'api-demo';
 
-  images:any = []
-  // private
-  constructor(private data: DataService){}
+  constructor(){}
 
   ngOnInit(): void {
-      this.data.getData().subscribe((response) =>{
-        this.images = response;
-        console.log(this.images);
-      })
+
   }
 }
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = 'api-demo';
 
-  constructor() { }
+  images:any = []
+  // private
+  constructor(private data: DataService){}
 
   ngOnInit(): void {
+      this.data.getData().subscribe((response) =>{
+        this.images = response;
+        console.log(this.images);
+      })
   }
-
 }
