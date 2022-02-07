5bfc0f067d34afc0b73a2de420e4666abfc6bf63
DevEnock <enockomondi305@gmail.com>
2022-02-07 17:35:43 +0300

    adding zoo animals api

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,6 +9,6 @@ export class DataService {
   constructor(private http:HttpClient) { }
 
   getData(){
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get('https://zoo-animal-api.herokuapp.com/animals/rand/10');
   }
 }
