fd77237ca801a3b376ae0ffc2d01bf7d22d3bd50
DevEnock <enockomondi305@gmail.com>
2022-02-22 13:56:55 +0300

    removing unused code snippets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
-import { AboutComponent } from './component/about/about.component';
+// import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
 import { ProductComponent } from './component/product/product.component';
 
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'home/product', component: ProductComponent},
-  {path: 'about', pathMatch: 'prefix', component: AboutComponent},
+  // {path: 'about', pathMatch: 'prefix', component: AboutComponent},
   {path: 'contact',pathMatch: 'prefix', component: ContactComponent},
 ];
 
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -12,10 +12,10 @@ export class NavComponent implements OnInit {
       link: 'Home',
       route: '/'
     },
-    {
-      link:'About',
-      route: '/about'
-    },
+    // {
+    //   link:'About',
+    //   route: '/about'
+    // },
     {
       link:'Contact',
       route: '/contact'
